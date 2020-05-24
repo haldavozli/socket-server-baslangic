177073b14c39eb6205c9458a99266818f2342eea
haldavozli <65831280+haldavozli@users.noreply.github.com>
2020-05-24 18:52:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-io.on('connection', function(socket){
-  socket.emit('welcome', {message: "Hi, Enes!"});
-
-  console.log('a user connected');
-
-
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, () => {
-  console.log('listening on *:3000');
-}); 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
