05b5d2e95251df59b11793d97fe99e32a626716f
haldavozli <65831280+haldavozli@users.noreply.github.com>
2020-05-24 19:15:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,17 @@
+  
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
-socket.on('send-nickname', function(nickname) {
-    socket.nickname = nickname;
-    users.push(socket.nickname);
-    console.log(users);
-});
-
-    socket.on('disconnect', function() {
-        socket.broadcast.to(socket.room).emit('notice', socket.username + ' has left the room');
-        users.splice(users.indexOf(socket.username), 1);
-        socket.emit('showRooms', rooms);
-    });
-
-    socket.on('message', function(data) {
-        socket.broadcast.to(socket.room).emit('message', data);
-    });
-
-    socket.on('createRoom', function(room) {
-        socket.leave(socket.room);
-        socket.room = room;
-        rooms.push(socket.room);
-        socket.join(socket.room);
-        socket.emit('showRooms', rooms);
-        console.log('Rooms: ' + rooms);
-        socket.broadcast.to(socket.room).emit('notice', socket.username + ' has joined to room');
-    });
-
-    socket.on('connectToRoom', function(room) {
-        console.log('Will connect to that room: ' + room);
-        socket.join(room);
-    });
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(port, function(){
