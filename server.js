4a3b83e8d263f2bf090829a1bea4c56822af3d77
haldavozli <65831280+haldavozli@users.noreply.github.com>
2020-05-24 18:19:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
-io.on('connection', function(socket){
-  socket.emit('welcome', {message: "Hi, Enes!"})
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, () => {
-  console.log('listening on *:3000');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
